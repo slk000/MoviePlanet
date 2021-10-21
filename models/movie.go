75aabf1f5f4b8e4d2b260e7718fd90b30bed9c95
slk000 <me@lo-li.net>
2021-10-21 19:32:37 +0800

    add fields in movie model

--- models/movie.go
+++ models/movie.go
@@ -9,6 +9,8 @@ type Movie struct {
 	PosterURL string    `json:"poster"`
 	Directors string    `json:"directors"`
 	Actors    string    `json:"actors"`
+	ReleaseDate time.Time `json:"release_date"`
+	Intro string `json:"intro"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
@@ -25,6 +27,8 @@ func (movie *Movie) ResponseMap() map[string]interface{} {
 	response["poster"] = movie.PosterURL
 	response["directors"] = movie.Directors
 	response["actors"] = movie.Actors
+	response["intro"] = movie.Intro
+	response["release_date"] = movie.ReleaseDate
 
 	return response
 }
